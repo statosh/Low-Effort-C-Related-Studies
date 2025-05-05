47b5da26442112be4b36b001387ac1c295891644
statosh <vlad555335@yandex.ru>
2025-05-05 15:53:02 +0300

    C# Appeared

---
+++ CSharp/05.05/05.05/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05._05
+{
+    class Program
+    {
+        static void Main(string[] args){
+            string name;
+            name = Console.ReadLine();
+        }
+    }
+}
