d6e878d7617eaf2c5680bc95f4b0e710db214c91
statosh <vlad555335@yandex.ru>
2025-06-27 15:55:02 +0300

    26_06_02

---
+++ WPF/WpfDataBindingDemo/WpfDataBindingDemo/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WpfDataBindingDemo {
+    public partial class MainWindow : Window {
+        public string FirstName { get; set; } = "Иван";
+        public string LastName { get; set; } = "Петров";
+
+        public MainWindow() {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void BackgroundColorCombo_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            if (BackgroundColorCombo.SelectedItem is ComboBoxItem item) {
+                string color = item.Content.ToString();
+                this.Background = (SolidColorBrush)new BrushConverter().ConvertFromString(color);
+            }
+        }
+
+        private void ThemeToggle_Checked(object sender, RoutedEventArgs e) {
+            this.Background = Brushes.DarkGray;
+        }
+
+        private void ThemeToggle_Unchecked(object sender, RoutedEventArgs e) {
+            this.Background = Brushes.LightGray;
+        }
+    }
+}
---
+++ WPF/WpfDataBindingDemo/WpfDataBindingDemo/SliderToColorConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WpfDataBindingDemo {
+    public class SliderToColorConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            byte intensity = (byte)(double)value;
+            return Color.FromRgb(intensity, 100, 150);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
