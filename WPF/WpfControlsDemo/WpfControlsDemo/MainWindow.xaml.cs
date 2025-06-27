2ae94abe7fa38abd969b452663f15a970722ffab
statosh <vlad555335@yandex.ru>
2025-06-27 15:34:30 +0300

    26_06

---
+++ WPF/WpfControlsDemo/WpfControlsDemo/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfControlsDemo {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e) {
+            if (sender is RadioButton rb) {
+                ResultLabel.Content = $"Вы выбрали: {rb.Content}";
+            }
+        }
+
+        private void CheckBox_Changed(object sender, RoutedEventArgs e) {
+            string options = "Выбраны опции: ";
+
+            if (OptionACheckBox.IsChecked == true) options += "A ";
+            if (OptionBCheckBox.IsChecked == true) options += "B ";
+            if (OptionCCheckBox.IsChecked == true) options += "C ";
+
+            if (options == "Выбраны опции: ") options = "Опции не выбраны";
+
+            ResultLabel.Content = options;
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e) {
+            Variant1RadioButton.IsChecked = false;
+            Variant2RadioButton.IsChecked = false;
+            Variant3RadioButton.IsChecked = false;
+
+            OptionACheckBox.IsChecked = false;
+            OptionBCheckBox.IsChecked = false;
+            OptionCCheckBox.IsChecked = false;
+
+            ResultLabel.Content = "Сброс выполнен";
+        }
+    }
+}
